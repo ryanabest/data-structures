2fbc4c385eb0940adfad6692618d6ef750a1216c
ryanabest <ryanabest@gmail.com>
2018-11-27 23:29:36 -0500

    Update homepage structure

--- final/routes/index.js
+++ final/routes/index.js
@@ -5,13 +5,12 @@ const { Client } = require('pg');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , body:
+  res.render('index', { title: 'DS2018 - Ryan Best' , body:
   `
   <a href="/endpoints"><h1>Endpoints</h1></a>
-  <h1>Front-End Visualizations</h1>
-  <ul>
-    <li><a href="/aa">AA Meetings</a></li>
-  </ul>
+  <a href="/aa"><h1>AA Meetings</h1></a>
+  <a href="/diary"><h1>Dear Diary</h1></a>
+  <a href="/sensor"><h1>Sensor</h1></a>
   `});
 });
 
