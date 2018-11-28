2f005135de505a3c6a15790cca0d25aac1225cd3
ryanabest <ryanabest@gmail.com>
2018-11-27 23:30:06 -0500

    Nest AA locations/meetings/schedules and add Diary endpoint

--- final/routes/endpoints.js
+++ final/routes/endpoints.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const { Client } = require('pg');
 
-// AWS Connection
+// AWS RDS Connection
 function createClient() {
   const client = new Client({
     user: 'ryanabest',
@@ -18,7 +18,7 @@ function createClient() {
 
 /* GET endpoints */
 router.get('/', function(req, res, next) {
-  res.render('endpoints', { title: 'Express' , body:
+  res.render('endpoints', { title: 'DS2018 Endpoints - Ryan Best' , body:
   `
   <h1>Endpoints</h1>
   <ul>
@@ -33,74 +33,109 @@ router.get('/', function(req, res, next) {
 router.get('/aa', function(req, res, next) {
   let query = `
   WITH TODAY as (
-  SELECT
-     TRIM(UPPER(to_char(the_day, 'day'))) as day
-    ,min(date_trunc('minute',the_day)::time) as earliest_time
-    ,max(date_trunc('minute',the_day)::time) as latest_time
-  FROM generate_series(CURRENT_TIMESTAMP
-                     ,CURRENT_TIMESTAMP + INTERVAL '48 hours'
-                     , interval  '1 minute') the_day
-  GROUP BY TRIM(UPPER(to_char(the_day, 'day')))
-  )
+    SELECT
+       TRIM(UPPER(to_char(the_day, 'day'))) as day
+      ,min(date_trunc('minute',the_day)::time) as earliest_time
+      ,max(date_trunc('minute',the_day)::time) as latest_time
+    FROM generate_series(CURRENT_TIMESTAMP
+                       ,CURRENT_TIMESTAMP + INTERVAL '48 hours'
+                       , interval  '1 minute') the_day
+    GROUP BY TRIM(UPPER(to_char(the_day, 'day')))
+    )
 
-  , MEETINGS as (
-   SELECT
-      f.meetingday
-     ,(RIGHT('0' || date_part('hour',f.meetingstarttime::time)::int,2) || ':' || RIGHT('0' || date_part('minute',f.meetingstarttime::time),2) || ':00')::time as meetingstarttime
-     -- ,date_part('hour',f.meetingstarttime::time)::int as meetingStartHour
-     -- ,date_part('minute',f.meetingstarttime::time)::int as meetingStartMin
-     ,(RIGHT('0' || date_part('hour',f.meetingendtime::time)::int,2) || ':' || RIGHT('0' || date_part('minute',f.meetingendtime::time),2) || ':00')::time as meetingendtime
-     -- ,date_part('hour',f.meetingendtime::time)::int as meetingEndHour
-     -- ,date_part('minute',f.meetingendtime::time)::int as meetingEndMin
-     ,split_part(f.meetingtype,' = ',2) as meetingtype
-     ,f.meetingSpecialInterest
-     ,l.*
-     ,g.*
-     ,m.*
-   FROM public.factMeetingSchedule f
-   JOIN dimLocation l
-      ON f.locationid = l.locationid
-   JOIN dimGeoLocation g
-      ON f.geolocationid = g.geolocationid
-   JOIN dimMeeting m
-      ON f.meetingID = m.meetingID
-  )
+    , MEETINGS as (
+     SELECT
+        f.meetingday
+       ,(RIGHT('0' || date_part('hour',f.meetingstarttime::time)::int,2) || ':' || RIGHT('0' || date_part('minute',f.meetingstarttime::time),2) || ':00')::time as meetingstarttime_raw
+       -- ,date_part('hour',f.meetingstarttime::time)::int as meetingStartHour
+       -- ,date_part('minute',f.meetingstarttime::time)::int as meetingStartMin
+       ,(RIGHT('0' || date_part('hour',f.meetingendtime::time)::int,2) || ':' || RIGHT('0' || date_part('minute',f.meetingendtime::time),2) || ':00')::time as meetingendtime_raw
+       -- ,date_part('hour',f.meetingendtime::time)::int as meetingEndHour
+       -- ,date_part('minute',f.meetingendtime::time)::int as meetingEndMin
+       ,split_part(f.meetingtype,' = ',2) as meetingtype
+       ,f.meetingSpecialInterest
+       ,f.meetingstarttime
+       ,f.meetingendtime
+       ,l.*
+       ,g.*
+       ,m.meetingid
+       ,REPLACE(initcap(m.meetingname),'''S','''s') as meetingname
+       ,m.meetingsubname
+       ,m.meetingdetails
+     FROM public.factMeetingSchedule f
+     JOIN dimLocation l
+        ON f.locationid = l.locationid
+     JOIN dimGeoLocation g
+        ON f.geolocationid = g.geolocationid
+     JOIN dimMeeting m
+        ON f.meetingID = m.meetingID
+    )
 
-  , TODAYS_MEETINGS as (
-  SELECT *, lldistance(m.lat,m.lon,40.7353003,-73.9967813) as lldistance
-  FROM MEETINGS m
-  JOIN TODAY t
-    ON UPPER(m.meetingday) LIKE '%'||t.day||'%'
-    AND t.earliest_time <= m.meetingstarttime
-    AND t.latest_time >= m.meetingstarttime
-  WHERE lldistance(m.lat,m.lon,40.7353003,-73.9967813) <= 2
-  )
+    , TODAYS_MEETINGS as (
+    SELECT *, lldistance(m.lat,m.lon,40.7353003,-73.9967813) as lldistance
+    FROM MEETINGS m
+    JOIN TODAY t
+      ON UPPER(m.meetingday) LIKE '%'||t.day||'%'
+      AND t.earliest_time <= m.meetingstarttime_raw
+      AND t.latest_time >= m.meetingstarttime_raw
+    WHERE lldistance(m.lat,m.lon,40.7353003,-73.9967813) <= 2
+    )
 
-  , TODAYS_MEETING_LOCATIONS as (
-  SELECT DISTINCT
-     geolocationid
-    ,locationid
-    ,locationname
-    ,formattedaddress
-    ,locationaddressdetail
-    ,lat
-    ,lon
-    ,lldistance
-    ,meetingid
-   FROM TODAYS_MEETINGS
-   )
+    , TODAYS_MEETING_SCHEDULE as (
+    SELECT
+      geolocationid
+     ,formattedaddress
+     ,lat
+     ,lon
+     ,lldistance
+     ,locationid
+     ,locationname
+     ,locationaddress
+     ,locationaddressdetail
+     ,meetingid
+     ,case when UPPER(meetingname) = UPPER(meetingsubname) OR meetingsubname = '' then meetingname else CONCAT(meetingname,': ',meetingsubname) end as meetingname
+     ,meetingdetails
+     ,json_agg(json_build_object('meeting_day',meetingday,'meeting_starttime',meetingstarttime,'meeting_endtime',meetingendtime,'meeting_type',meetingtype,'meeting_specialinterest',meetingspecialinterest)) as schedule_json
+     --,array_agg(DISTINCT locationid) as locationid_list
+     --,array_agg(DISTINCT meetingid) as meetingid_list
+    FROM TODAYS_MEETINGS
+    GROUP BY geolocationid,formattedaddress,lat,lon,lldistance,locationid,locationname,locationaddress,locationaddressdetail,meetingid,meetingname,meetingsubname,meetingdetails
+    ORDER BY lldistance ASC
+     )
 
-  SELECT
-    geolocationid
-   ,formattedaddress
-   ,lat
-   ,lon
-   ,lldistance
-   ,array_agg(DISTINCT locationid) as locationid_list
-   ,array_agg(DISTINCT meetingid) as meetingid_list
-  FROM TODAYS_MEETING_LOCATIONS
-  GROUP BY geolocationid,formattedaddress,lat,lon,lldistance
-  ORDER BY lldistance ASC;
+     ,TODAYS_MEETING_LOCATION as (
+     SELECT
+        geolocationid
+       ,formattedaddress
+       ,lat
+       ,lon
+       ,lldistance
+       ,locationid
+       ,locationname
+       ,locationaddress
+       ,locationaddressdetail
+       ,json_agg(json_build_object('meeting_id',meetingid,'meeting_name',meetingname,'meeting_details',meetingdetails,'schedule',schedule_json)) as meetings_json
+     FROM TODAYS_MEETING_SCHEDULE
+     GROUP BY geolocationid,formattedaddress,lat,lon,lldistance,locationid,locationname,locationaddress,locationaddressdetail
+     ORDER BY lldistance ASC
+     )
+
+     ,TODAYS_MEETING_GEOLOCATION as (
+     SELECT
+        geolocationid
+       ,formattedaddress
+       ,lat
+       ,lon
+       ,lldistance
+       ,json_agg(json_build_object('location_id',locationid,'location_name',locationname,'location_address',locationaddress,'location_addressdetail',locationaddressdetail,'meetings',meetings_json)) as locations_json
+     FROM TODAYS_MEETING_LOCATION
+     GROUP BY geolocationid,formattedaddress,lat,lon,lldistance
+     ORDER BY lldistance ASC
+     )
+
+    SELECT
+    *
+    FROM TODAYS_MEETING_GEOLOCATION
   `
 
   const client = createClient();
@@ -117,6 +152,40 @@ router.get('/aa', function(req, res, next) {
 
 // Diary Endpoint
 router.get('/diary', function(req, res, next) {
+  // AWS Dynamo
+  const AWS = require('aws-sdk');
+  AWS.config = new AWS.Config();
+  AWS.config.accessKeyId = process.env.AWS_ID;
+  AWS.config.secretAccessKey = process.env.AWS_KEY;
+  AWS.config.region = "us-east-1";
+  const dynamodb = new AWS.DynamoDB();
+
+  // DynamoDB (NoSQL) query
+  let params = {
+      TableName : "deardiary_viz",
+      KeyConditionExpression: `
+        #typ = :topicName
+        and #dt between :minDate and :maxDate`, // the query expression
+      ExpressionAttributeNames: { // name substitution, used for reserved words in DynamoDB
+          "#typ" : "type",
+          "#dt"  : "date"
+      },
+      ExpressionAttributeValues: { // the query values
+          ":topicName": {S: "interactive viz"},
+          ":minDate": {S: new Date("October 1, 2018").toLocaleDateString()},
+          ":maxDate": {S: new Date("December 31, 2018").toLocaleDateString()}
+      }
+  };
+
+  dynamodb.query(params, function(err, data) {
+      if (err) {
+          console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+      }
+      else {
+          res.send(data.Items);
+          console.log('Responded to Diary Request');
+      }
+  });
 })
 
 // Sensor Endpoint
