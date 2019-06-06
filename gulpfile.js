b14f121cf74a2725e0114089a0e785a13745e05c
ryanabest <ryanabest@gmail.com>
2019-06-06 09:45:57 -0400

    update security vunerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var bs = require('browser-sync').create();
-var sass = require('gulp-sass');
 
 gulp.task('browser-sync', function () {
   bs.init({
@@ -10,17 +9,9 @@ gulp.task('browser-sync', function () {
   });
 });
 
-gulp.task('sass', function () {
-    return gulp.src('scss/*.scss')
-                .pipe(sass())
-                .pipe(gulp.dest('css'))
-                .pipe(bs.reload({stream: true}));
-});
-
-gulp.task('watch', ['browser-sync'], function () {
-    gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', bs.reload);
-    gulp.watch("**/*.html").on('change', bs.reload);
-    gulp.watch("css/*.css").on('change', bs.reload);
-    gulp.watch("js/*.js").on('change', bs.reload);
-});
+gulp.task('watch', gulp.parallel('browser-sync', function() {
+  gulp.watch("*.html").on('change', bs.reload);
+  gulp.watch("**/*.html").on('change', bs.reload);
+  gulp.watch("css/*.css").on('change', bs.reload);
+  gulp.watch("js/*.js").on('change', bs.reload);
+}));
