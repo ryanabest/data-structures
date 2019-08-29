61fb63532baebeefc9db0e10831cb438ad7562f3
ryanabest <ryanabest@gmail.com>
2019-08-29 14:22:13 -0400

    add tutor week1

--- week1/week1.js
+++ week1/week1.js
@@ -3,6 +3,7 @@
 
 var request = require('request');
 var fs = require('fs');
+var async = require('async');
 
 let textFiles = [
    "https://parsons.nyc/aa/m01.html"
@@ -17,19 +18,26 @@ let textFiles = [
   ,"https://parsons.nyc/aa/m10.html"
 ]
 
-// request(textFile, function(error, response, body){
-//     if (!error && response.statusCode == 200) {
-//         fs.writeFileSync('data/thesis.txt', body);
-//     }
-//     else {console.log("Request failed!")}
-// });
 
+// ASYNC METHOD
+async.each(textFiles,function(textFile,callback) {
+  let newFileName = textFile.split("/")[textFile.split("/").length-1]
+  request(textFile, function(error, response, body){
+      if (!error && response.statusCode == 200) {
+        fs.writeFileSync('/Users/ryanbest/Documents/Parsons/ds-2018/data-structures/data/raw/' + newFileName, body);
+      }
+      else {console.log("Request failed!")}
+    });
+})
+
+// CLOSURE METHOD WITH LET
 for (let i=0;i<textFiles.length;i++) {
   let textFile = textFiles[i]
   let newFileName = textFile.split("/")[textFile.split("/").length-1]
   request(textFile, function(error, response, body){
       if (!error && response.statusCode == 200) {
-          fs.writeFileSync('/Users/ryanbest/Documents/Parsons/ds-2018/data-structures/data/raw/' + newFileName, body);
+        // console.log(body);
+        fs.writeFileSync('/Users/ryanbest/Documents/Parsons/ds-2018/data-structures/data/raw/' + newFileName, body);
       }
       else {console.log("Request failed!")}
   });
